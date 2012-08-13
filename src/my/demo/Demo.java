8225e6b9b9776f7c7e22132024ce57ab7ce3521a
zhonglihua <lihua.zhong@dragonflow.com>
2012-08-13 15:37:16 +0800

    DEMO COOMMIT

---
+++ src/my/demo/Demo.java
@@ -0,0 +1,13 @@
+package my.demo;
+
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("DEMO");
+	}
+
+}
